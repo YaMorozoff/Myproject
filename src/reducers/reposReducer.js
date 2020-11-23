e27bae7c3d6f64a2b63f194445457dbbd8fc85db
YaMorozoff <48440240+YaMorozoff@users.noreply.github.com>
2020-11-23 20:31:04 +0300

    1

---
+++ src/reducers/reposReducer.js
@@ -0,0 +1,17 @@
+
+
+const defaultState={
+    items:[],
+    isFetching: true,
+}
+
+
+export default function reposReducer(state, action){
+    switch (action.type){
+
+        default:
+            return state
+    }
+
+
+}
